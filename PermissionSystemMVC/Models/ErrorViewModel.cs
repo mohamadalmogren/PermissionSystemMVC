cbca900a0ed5ca704daea6354779431ca12080a1
mohamadalmogren <mohamadalmogren@gmail.com>
2021-07-28 11:37:05 +0300

    Add project files.

---
+++ PermissionSystemMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PermissionSystemMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
